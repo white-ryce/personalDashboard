ae48845d6423732c9ca2a640411a06e28477b032
white-ryce <ryanrabena10@gmail.com>
2023-03-06 19:50:38 -0800

    Added Accurate date and time

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import {transition, trigger} from '@angular/animations';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {
   style,animate
 } from '@angular/animations';
 import { fader } from './app-routing.module';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -12,10 +13,19 @@ import { fader } from './app-routing.module';
   styleUrls: ['./app.component.scss'],
   animations: [fader]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 
   prepareRoute(outlet: RouterOutlet)
   {
     return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
   } 
+
+  dateTime: Date
+
+  ngOnInit(): void 
+  {
+     timer(0,1000).subscribe(() => {
+      this.dateTime = new Date()
+     })
+  }
 }
